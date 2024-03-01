fb27bb6f942693215657d7f682ae74d6e3b74e6d
VitorKubica <vitor05kubica12@gmail.com>
2024-03-01 10:57:16 -0300

    Projeto transformado para tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import Cabecalho from '../components/Cabecalho/Cabecalho'
-import './page.scss'
 import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-screen flex flex-col justify-center items-center bg-gray-50">
       <Cabecalho/>
-      <section className="conteudo">
-        <div className="elemento">
-          <div className="icone"></div>
-        </div>
-        <div className="titulo">
-          <h1>Redefinir Senha</h1>
-          <p>Digite seu email para receber o código de recuperação</p>
-        </div>
-        <div className="linha"></div>
-        <div className="emailform">
-          <form action="">
-            <label htmlFor="email">Email</label>
-            <input type="text" placeholder="exemple@email.com" />
-          </form>
-          <button className="buttonForm"></button>
-          </div>
-      </section>
-      <div className="linkCadas">
-        <p>Ainda não possui conta?<Link href="/">Cadastre-se</Link></p>
-      </div>
-    </main>
+  <section className="conteudo border border-gray-200 rounded-lg w-full max-w-sm py-4 flex flex-col items-center">
+    <div className="elemento flex justify-left w-full">
+      <div className="icone w-10 h-10 rounded-full bg-green-400 ml-8"></div>
+    </div>
+    <div className="titulo text-initial mb-4 ml-8">
+      <h1 className="text-2xl font-bold text-gray-700">Redefinir Senha</h1>
+      <p className="text-gray-700">Digite seu email para receber o código de recuperação</p>
+    </div>
+    <div className="linha h-0.5 w-full bg-gray-200 mb-4"></div>
+    <div className="emailform w-full flex justify-center items-end">
+      <form className="flex flex-col">
+        <label for="email" className="text-gray-700 mb-2">Email</label>
+        <input type="text" id="email" placeholder="exemple@email.com" className="border border-gray-200 rounded-lg w-full p-2"/>
+      </form>
+      <button type="button" className="w-10 h-10 rounded-full bg-green-400 mr-1 ml-2"></button>
+    </div>
+  </section>
+  <div className="text-center mt-4 flex justify-center items-center">
+    <p className="text-gray-700">Ainda não possui conta? <Link href="/" className="text-green-500 hover:text-green-600">Cadastre-se</Link></p>
+  </div>
+</main>
+
   );
 }
